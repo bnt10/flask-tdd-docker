768a3fb1d2a685cd2adaca02ecccf44c501d16d9
jin <bnt10@naver.com>
2022-02-24 22:22:35 +0900

    start tdd project

---
+++ src/config.py
@@ -0,0 +1,12 @@
+class BaseConfig:
+    TESTING = False
+
+class DevelopmentConfig(BaseConfig):
+    pass
+
+class TestConfig(BaseConfig):
+    TESTING = True
+
+class ProductionConfig(BaseConfig):
+    pass
+    
